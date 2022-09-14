b453ebfa02c65b7dd2daa9d01a3fbc99252f3b8e
Diego Apolinario <dieego.apolinario@gmail.com>
2022-09-14 18:47:12 -0300

    commit initial

---
+++ send-email/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import emailjs from "@emailjs/browser";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  methods: {
+    sendEmail() {
+      emailjs
+        .sendForm(
+          "gmailMessage",
+          "template_kofh0hj",
+          this.$refs.form,
+          "78-lxq-AdGs9ocHrg"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ send-email/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name" /><br /><br />
+    <label>Email</label>
+    <input type="email" name="user_email" /><br /><br />
+    <label>Message</label>
+    <textarea name="message"></textarea><br /><br />
+    <input type="submit" value="Send" />
+  </form>
+</template>
